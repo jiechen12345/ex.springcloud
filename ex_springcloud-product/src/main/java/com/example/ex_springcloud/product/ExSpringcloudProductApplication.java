59e0ad1ad289cfbd83d20962bf021288a3244732
JieChen <cheners123@gmail.com>
2022-09-11 21:05:38 +0800

    gen CRUD

--- ex_springcloud-product/src/main/java/com/example/ex_springcloud/product/ExSpringcloudProductApplication.java
+++ ex_springcloud-product/src/main/java/com/example/ex_springcloud/product/ExSpringcloudProductApplication.java
@@ -3,6 +3,7 @@ package com.example.ex_springcloud.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@MapperScan("com.example.ex_springcloud.product.dao")
 @SpringBootApplication
 public class ExSpringcloudProductApplication {
 
--- ex_springcloud-product/src/test/java/com/example/ex_springcloud/product/ExSpringcloudProductApplicationTests.java
+++ ex_springcloud-product/src/test/java/com/example/ex_springcloud/product/ExSpringcloudProductApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.ex_springcloud.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.ex_springcloud.product.entity.BrandEntity;
+import com.example.ex_springcloud.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ExSpringcloudProductApplicationTests {
+    @Autowired
+    BrandService brandServicel;
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setName("HTC");
+//        brandServicel.save(brandEntity);
+
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setDescript("haha..");
+//        brandServicel.updateById(brandEntity);
+
+       List<BrandEntity>brandEntities= brandServicel.list(new QueryWrapper<BrandEntity>().eq("brand_id",1));
+       brandEntities.forEach(item->{
+           System.out.println(item);
+       });
+        System.out.println("OK....");
     }
 
 }
